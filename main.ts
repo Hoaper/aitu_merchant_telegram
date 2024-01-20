e367d558ca2afe21aac36bd523c79490e36e140e
rakhat_cyanid <zhayshilik@gmail.com>
2024-01-20 21:53:10 +0600

    init

---
+++ main.ts
@@ -0,0 +1,30 @@
+import { Context, Markup, Telegraf } from "telegraf"
+require('dotenv').config()
+
+const WEB_APP_URL = "https://github.com/"
+const bot = new Telegraf(process.env.BOT_TOKEN || "")
+
+bot.start(async (ctx) => {
+    ctx.reply(
+		"Launch mini app from inline keyboard!",
+		Markup.inlineKeyboard([
+            Markup.button.webApp("open", WEB_APP_URL)
+        ])
+	)
+    ctx.setChatMenuButton({
+        "text": "SHOP",
+        type: "web_app",
+        "web_app": {
+            "url": WEB_APP_URL
+        }
+    })
+
+})
+
+
+
+bot.launch()
+    .then(() => {
+        console.log("BOT STARTED");
+})
+
